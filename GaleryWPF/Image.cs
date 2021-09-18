b28e29f82f169ae4659acb1979052f7ba1dc08c7
nicat ceferli <mega.ceferli@gmail.com>
2021-09-18 23:52:24 +0400

    Add project files.

---
+++ GaleryWPF/Image.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace GaleryWPF
+{
+    public class MyImage
+    {
+        public string CreatedTime { get; set; }
+        public string ImageSource { get; set; }
+        public string ImageName { get; set; }
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public MyImage(string name, int size, string type,string Path)
+        {
+            var temp = DateTime.Now;
+            CreatedTime = $"{temp.ToShortDateString()} {temp.ToShortTimeString()}";
+            ImageName = name;
+            Size = $"{size}MB";
+            Type = type;
+            ImageSource = Path;
+
+
+        }
+        public MyImage()
+        {
+
+            var temp = DateTime.Now;
+            CreatedTime = $"{temp.ToShortDateString()} {temp.ToShortTimeString()}";
+
+        }
+
+    }
+}
---
+++ GaleryWPF/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GaleryWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+
+    {
+        public ObservableCollection<MyImage> Images { get; set; }
+        public ObservableCollection<TilesImageUC> MyPropertyes { get; set; }
+
+
+
+        public Image MyProperty
+        {
+            get { return (Image)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(Image), typeof(MainWindow));
+
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            DataContext = this;
+            //Images = new ObservableCollection<MyImage>();
+
+            List<string> Types = new List<string>();
+            Types.Add("PNG File");
+            Types.Add("JPG File");
+            Types.Add("TIFF File");
+            Types.Add("BMP File");
+            Types.Add("RAF File");
+            Types.Add("JPEG File");
+
+
+            var CreateImages = new Bogus.Faker<MyImage>()
+                .RuleFor(I => I.ImageName, I => I.Name.FullName())
+                .RuleFor(I => I.Size, I => $"{I.Random.Number(0, 1000)}MB")
+                .RuleFor(I => I.Type, I => $"{Types[I.Random.Int(0, Types.Count - 1)]}")
+                .RuleFor(I => I.ImageSource, I => I.Image.Image(170, 185, true, false)).Generate(10);
+
+            Images = new ObservableCollection<MyImage>(CreateImages);
+            MyPropertyes = new ObservableCollection<TilesImageUC>();
+
+            MyProperty = new Image();
+            MyProperty.Source = new BitmapImage(new Uri(Images[1].ImageSource));
+
+
+
+            for (int i = 0; i < Images.Count; i++)
+            {
+                var temp = new TilesImageUC();
+                temp.ImageSource = Images[i];
+                MyPropertyes.Add(temp);
+            MyPropertyes[i].Width = 180;
+            MyPropertyes[i].Height = 185;
+
+            }
+
+
+
+
+            //Images.Add(new MyImage("NoBody",300,"PNG"));
+
+
+        }
+
+        private void MenuTiles_Click(object sender, RoutedEventArgs e)
+        {
+
+            Panel.SetZIndex(ViewTiles,1);
+            Panel.SetZIndex(ViewDetails,0);
+        }
+
+        private void MenuDetails_Click(object sender, RoutedEventArgs e)
+        {
+            Panel.SetZIndex(ViewTiles, 0);
+            Panel.SetZIndex(ViewDetails, 1);
+        }
+    }
+}
